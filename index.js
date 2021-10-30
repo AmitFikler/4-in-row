bb62d440708c9035359fdb5eef1de5464c5627eb
AmitFikler <amitfikler@gmail.com>
2021-10-30 12:36:50 +0300

    add class Controller

--- index.js
+++ index.js
@@ -117,3 +117,26 @@ class View {
       this.message.innerHTML = "It's a draw!";
     }
 }
+
+
+class Controller {
+    constructor() {
+      this.model = new Model();
+      this.view = new View();
+  
+      this.view.playEvent.addListener(move => { this.model.play(move); });
+  
+      this.model.updateCellEvent.addListener(data => { this.view.updateCell(data); });
+      this.model.victoryEvent.addListener(winner => { this.view.victory(winner); });
+      this.model.drawEvent.addListener(() => { this.view.draw(); });
+    }
+  
+    run() {
+      this.view.render();
+    }
+}
+
+
+const app = new Controller();
+
+app.run();
